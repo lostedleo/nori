aeacb2322220498233575d6b52e0de330eab6cd8
zhuzhenwei.leo <zhuzhenwei.leo@bytedance.com>
2021-07-01 21:32:19 +0800

    update third_party libraries

--- third_party/cmake/Versions.cmake
+++ third_party/cmake/Versions.cmake
@@ -6,12 +6,12 @@ set(GFLAGS_VERSION        "2.2.2")
 set(GFLAGS_HASH           "SHA256=34af2f15cf7367513b352bdcd2493ab14ce43692d2dcd9dfc499492966c64dcf")
 
 # glog
-set(GLOG_VERSION          "0.3.5")
-set(GLOG_HASH             "SHA256=54ecddaf9674dea92881395ab4b46387b20602bfafd1fb42784b61a11ab4e462")
+set(GLOG_VERSION          "0.5.0")
+set(GLOG_HASH             "SHA256=eede71f28371bf39aa69b45de23b329d37214016e2055269b3b5e7cfd40b59f5")
 
 # googletest
-set(GOOGLETEST_VERSION    "1.8.0")
-set(GOOGLETEST_HASH       "SHA256=8e1d653bb3c2f95e5746cd996549f1ede9ca1bd50f5f3c3c06ef93eba21f2671")
+set(GOOGLETEST_VERSION    "1.11.0")
+set(GOOGLETEST_HASH       "SHA256=529363aa36b85f8515d05ca3e90e5e204b30f9367c7ace0f8ac38176b5dd5a8c")
 
 # gperftools
 set(GPERFTOOLS_VERSION    "2.9.1")
