17608721074cbcf642a977450fd5f1b8c45cd76b
zhenwei <losted.leo@gmail.com>
2022-11-27 15:49:55 +0800

    add sum and product unittest for balance

--- ai/test/balance_unittest.cc
+++ ai/test/balance_unittest.cc
@@ -12,12 +12,54 @@
 namespace nrai {
 
 TEST(BalanceTest, SimpleTest) {
+  // unitset for sum
   Balance<int> balance;
   ASSERT_EQ(-1, balance.x());
   ASSERT_EQ(1, balance.y());
   ASSERT_EQ(2, balance.connection());
-  ASSERT_FALSE(false);
-  ASSERT_TRUE(true);
+
+  balance.grow();
+  ASSERT_EQ(-2, balance.x());
+  ASSERT_EQ(2, balance.y());
+  ASSERT_EQ(4, balance.connection());
+
+  balance.set_coeff(100);
+  balance.grow();
+  ASSERT_EQ(-102, balance.x());
+  ASSERT_EQ(102, balance.y());
+  ASSERT_EQ(204, balance.connection());
+
+  balance.swap();
+  ASSERT_EQ(102, balance.x());
+  ASSERT_EQ(-102, balance.y());
+  ASSERT_EQ(-204, balance.connection());
+
+  balance.die();
+  ASSERT_EQ(-1, balance.x());
+  ASSERT_EQ(1, balance.y());
+  ASSERT_EQ(2, balance.connection());
+
+  // unittest for product
+  Balance<double> balance2(2, BT_PROD);
+  ASSERT_EQ(1, balance2.x());
+  ASSERT_EQ(1, balance2.y());
+  ASSERT_EQ(1, balance2.connection());
+
+  balance2.grow();
+  ASSERT_EQ(0.5, balance2.x());
+  ASSERT_EQ(2, balance2.y());
+  ASSERT_EQ(4, balance2.connection());
+
+  balance2.set_coeff(4);
+  balance2.grow();
+  ASSERT_EQ(0.125, balance2.x());
+  ASSERT_EQ(8, balance2.y());
+  ASSERT_EQ(64, balance2.connection());
+
+  balance2.die();
+  ASSERT_EQ(1, balance2.x());
+  ASSERT_EQ(1, balance2.y());
+  ASSERT_EQ(1, balance2.connection());
 }
 
 TEST(BalanceTest, ComplexTest) {
