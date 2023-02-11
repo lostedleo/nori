5b88779c96cd1281b98953800d0052b2ff973db2
zhuzhenwei <losted.leo@gmail.com>
2023-02-11 13:41:31 +0800

    modify nrai test add swap

--- ai/test/test_nrai.py
+++ ai/test/test_nrai.py
@@ -5,19 +5,25 @@ from _nrai import BalanceInt, BalanceFloat, BalanceDouble
 
 def main():
     balance1 = BalanceInt(2, 1)
-    print(balance1.x(), balance1.y(), balance1.connection())
+    print('balance1: ', balance1.x(), balance1.y(), balance1.connection())
     balance1.grow()
-    print(balance1.x(), balance1.y(), balance1.connection())
+    print('balance1: ', balance1.x(), balance1.y(), balance1.connection())
+    balance1.swap()
+    print('balance1: ', balance1.x(), balance1.y(), balance1.connection())
 
     balance2 = BalanceFloat(2, 2)
-    print(balance2.x(), balance2.y(), balance2.connection())
+    print('balance2: ', balance2.x(), balance2.y(), balance2.connection())
     balance2.grow()
-    print(balance2.x(), balance2.y(), balance2.connection())
+    print('balance2: ', balance2.x(), balance2.y(), balance2.connection())
+    balance2.swap()
+    print('balance2: ', balance2.x(), balance2.y(), balance2.connection())
 
     balance3 = BalanceDouble(2, 2)
-    print(balance3.x(), balance3.y(), balance3.connection())
+    print('balance3: ', balance3.x(), balance3.y(), balance3.connection())
     balance3.grow()
-    print(balance3.x(), balance3.y(), balance3.connection())
+    print('balance3: ', balance3.x(), balance3.y(), balance3.connection())
+    balance3.swap()
+    print('balance3: ', balance3.x(), balance3.y(), balance3.connection())
 
 if __name__ == '__main__':
     main()
